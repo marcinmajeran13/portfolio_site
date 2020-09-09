551a8495b660ebf798fb2302477f19c401dec265
Marcin <marcin.majeran13@gmail.com>
2020-09-09 17:50:12 +0200

    Visual updated

--- venv/mmajeran/base_app/forms.py
+++ venv/mmajeran/base_app/forms.py
@@ -1,25 +1,19 @@
-from django import forms
 from base_app.models import Post
+from django import forms
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text', 'type']
-        
+
         widgets = {
             'title': forms.TextInput(attrs={'class': 'textinputclass'}),
             'text': forms.Textarea(attrs={'class': 'editable medium-editor-textarea'}),
         }
 
+
 class ContactForm(forms.Form):
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'emailfield'}))
-    subject = forms.CharField(required=True,widget=forms.TextInput(attrs={'class':'subjectfield'}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class':'emailfield'}), required=True)
-
-
-
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#
-#     pass
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'emailfield'}))
+    subject = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'subjectfield'}))
+    message = forms.CharField(widget=forms.TextInput(attrs={'class': 'messagefield'}), required=True)
--- venv/mmajeran/base_app/models.py
+++ venv/mmajeran/base_app/models.py
@@ -2,26 +2,27 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 
+
 # Create your models here.
+
 
 class Post(models.Model):
     post_type = (('Repair', 'Repair'), ('Review', 'Review'))
 
-    author = models.CharField(default='Marcin Majeran',max_length=50)
+    author = models.CharField(default='Marcin Majeran', max_length=50)
     title = models.CharField(max_length=100)
     text = models.TextField()
     publish_date = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=32, choices=post_type, default='type')
-    
+
     def __str__(self):
         return self.title
-    
+
     def publish(self):
         self.save()
-    
+
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk':self.pk})
-        
-     
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
 # class Comment (models.Model):
 #     pass
