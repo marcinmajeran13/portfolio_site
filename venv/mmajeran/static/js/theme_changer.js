d4c8434eaa9284ef0bbdf7088f45dca80287fd97
Marcin <marcin.majeran13@gmail.com>
2020-09-16 11:18:42 +0200

    Visual updates

--- venv/mmajeran/static/js/theme_changer.js
+++ venv/mmajeran/static/js/theme_changer.js
@@ -1,10 +1,10 @@
 var spacegray = document.querySelector('#spacegray')
 var purple = document.querySelector('#purple')
 var green = document.querySelector('#green')
-var brown = document.querySelector('#brown')
+var mustard = document.querySelector('#mustard')
 var nav = document.querySelector('.navbar-custom')
 
-function change_background(color) {
+function change_background() {
     document.body.style.background = localStorage.getItem('theme_color')
     nav.style.backgroundColor = localStorage.getItem('nav_color')
     }
@@ -34,16 +34,16 @@ function adds() {
 
 
     purple.addEventListener('click', function(){ remember_color('#370752','#5f1c85')})
-    purple.addEventListener('click', function(){ change_background('#370752')})
+    purple.addEventListener('click', function(){ change_background()})
 
     spacegray.addEventListener('click', function(){ remember_color('#242625','#303030')})
-    spacegray.addEventListener('click', function(){ change_background('#242625')})
+    spacegray.addEventListener('click', function(){ change_background()})
 
     green.addEventListener('click', function(){ remember_color('#0f421f','#415e50')})
-    green.addEventListener('click', function(){ change_background('#0f421f')})
+    green.addEventListener('click', function(){ change_background()})
 
-    brown.addEventListener('click', function(){ remember_color('#614805','#997208')})
-    brown.addEventListener('click', function(){ change_background('#614805')})
+    mustard.addEventListener('click', function(){ remember_color('#614805','#ad9c6c')})
+    mustard.addEventListener('click', function(){ change_background()})
  }
 
 
