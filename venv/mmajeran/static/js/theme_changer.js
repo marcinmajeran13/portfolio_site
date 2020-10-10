3a4392b7a2fecb0ee20b1ecea3e1c2d5e4280640
Marcin <marcin.majeran13@gmail.com>
2020-10-10 15:52:32 +0100

    Added transitions, visual updates

--- venv/mmajeran/static/js/theme_changer.js
+++ venv/mmajeran/static/js/theme_changer.js
@@ -3,15 +3,37 @@ var purple = document.querySelector('#purple')
 var green = document.querySelector('#green')
 var mustard = document.querySelector('#mustard')
 var nav = document.querySelector('.navbar-custom')
+var title = document.querySelector('h1 a')
+var title2 = document.querySelectorAll('.page-wrapper h1')
+var post_title = document.querySelectorAll('.repair-title a')
+var app_name = document.querySelectorAll('.app-name')
+var btn = document.querySelector('.mybtn')
+var i
 
 function change_background() {
     document.body.style.background = localStorage.getItem('theme_color')
     nav.style.backgroundColor = localStorage.getItem('nav_color')
+    title.style.color = localStorage.getItem('title_col')
+
+    for (i=0; i < title2.length; i++) {
+        title2[i].style.color = localStorage.getItem('title_col')
+    }
+    
+    for (i=0; i < post_title.length; i++) {
+        post_title[i].style.color = localStorage.getItem('title_col')
     }
 
-function remember_color(color, navcol) {
+    for (i=0; i < app_name.length; i++) {
+        app_name[i].style.color = localStorage.getItem('title_col')
+    }
+
+     btn.style.backgroundColor = localStorage.getItem('title_col')
+    }
+
+function remember_color(color, navcol, title_col) {
     localStorage.setItem('theme_color', color)
     localStorage.setItem('nav_color', navcol)
+    localStorage.setItem('title_col', title_col)
 }
 
 document.querySelector('.top-page').addEventListener('mouseover', function(){
@@ -33,16 +55,16 @@ document.querySelector('.top-page').addEventListener('touchend', function(){
 function adds() {
 
 
-    purple.addEventListener('click', function(){ remember_color('#370752','#5f1c85')})
+    purple.addEventListener('click', function(){ remember_color('#370752','#5f1c85','#8140b3')})
     purple.addEventListener('click', function(){ change_background()})
 
-    spacegray.addEventListener('click', function(){ remember_color('#242625','#303030')})
+    spacegray.addEventListener('click', function(){ remember_color('#242625','#303030','#8140b3')})
     spacegray.addEventListener('click', function(){ change_background()})
 
-    green.addEventListener('click', function(){ remember_color('#0f421f','#415e50')})
+    green.addEventListener('click', function(){ remember_color('#0f421f','#415e50','#71ad90')})
     green.addEventListener('click', function(){ change_background()})
 
-    mustard.addEventListener('click', function(){ remember_color('#614805','#ad9c6c')})
+    mustard.addEventListener('click', function(){ remember_color('#614805','#ad9c6c','#d1bc82')})
     mustard.addEventListener('click', function(){ change_background()})
  }
 
